e3aaa713182295f41293f695a19505080227750e
meaccgive3-afk <meaccgive3@gmail.com>
2026-05-01 10:30:54 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,9 @@ importScripts('/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(sw.fetch(event));
 });
-
