c6119ca110a8f9885e4275a67e5051cbb6733d4a
meaccgive3-afk <meaccgive3@gmail.com>
2026-05-01 13:37:42 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,16 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv.config.js');
-importScripts('/uv/uv.sw.js');
+app.get('/sw.js', (req, res) => {
+  res.type('application/javascript').send(`
+    self.skipWaiting();
+    self.clients.claim();
 
-const sw = new UVServiceWorker();
+    importScripts('/uv/uv.bundle.js');
+    importScripts('/uv.config.js');
+    importScripts('/uv/uv.sw.js');
 
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+    const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(sw.fetch(event));
+    self.addEventListener('fetch', event => {
+      event.respondWith(sw.fetch(event));
+    });
+  `);
 });
