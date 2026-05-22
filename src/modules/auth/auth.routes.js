3dddc6de916badaab11a72634e4c54483d5a3068
meaccgive3-afk <meaccgive3@gmail.com>
2026-05-22 09:32:49 +0300

    Create  auth.routes.js

---
+++ src/modules/auth/auth.routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const authController = require('./auth.controller');
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+
+module.exports = router;
