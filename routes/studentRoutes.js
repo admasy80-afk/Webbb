bb5e5674dd0ee40f907ec6467348774227e21d21
meaccgive3-afk <meaccgive3@gmail.com>
2026-05-29 19:31:36 +0300

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,13 +4,15 @@ const studentController = require('../controllers/studentController');
 const { authenticateToken } = require('../middleware/auth');
 const { apiLimiter } = require('../middleware/rateLimiters');
 
+// Public routes
+router.post('/submit-quiz', studentController.submitQuiz);
+
+// Protected routes
 router.use(authenticateToken);
 
 router.post('/dashboard-data', apiLimiter, studentController.getDashboardData);
 router.get('/video/stream/:msgId', studentController.streamVideo);
-router.post('/submit-quiz', studentController.submitQuiz);
 router.post('/check-status', studentController.checkStatus);
 router.post('/verify-phone', studentController.verifyPhone);
 
 module.exports = router;
-
