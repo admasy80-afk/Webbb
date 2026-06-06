bff26e33a55b5fea5e88d67c256be51425ca012d
admasy80-afk <admasy80@gmail.com>
2026-06-06 20:21:21 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/authController.js');
+const authController = require('../controllers/​authController.js');
 const { loginLimiter } = require('../middleware/rateLimiters');
 const { authenticateToken } = require('../middleware/auth');
 
